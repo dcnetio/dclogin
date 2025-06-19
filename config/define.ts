2b2d86daa77e35554beb62e772d9f1c1d97add32
wangqin <752935464@qq.com>
2025-06-19 15:47:36 +0800

    常量

--- config/define.ts
+++ config/define.ts
@@ -1,8 +1,17 @@
 let _basePath = '/v0_0_4';
 let _apiUrl = 'https://wallet.dcnetio.com/api';
-if(process.env.NODE_ENV === 'development') {
+let _dcConfig = {
+    wssUrl: 'wss://dcchain.baybird.cn',
+    backWssUrl: 'wss://dcchain.baybird.cn',
+}
+if (typeof process !== 'undefined' && process.env && process.env['NODE_ENV'] === 'development') {
     _basePath = '';
     _apiUrl = 'http://localhost:9001/api';
+    _dcConfig = {
+        wssUrl: 'ws://192.168.31.31:9944',
+        backWssUrl: 'ws://192.168.31.31:9944',
+    }
 }
 export const basePath = _basePath;
 export const apiUrl = _apiUrl;
+export const dcConfig = _dcConfig;

79942b231673ffe7a18d241331823a31bd18c02b
wangqin <752935464@qq.com>
2025-06-20 18:33:07 +0800

    正式注释

--- config/define.ts
+++ config/define.ts
@@ -4,14 +4,14 @@ let _dcConfig = {
     wssUrl: 'wss://dcchain.baybird.cn',
     backWssUrl: 'wss://dcchain.baybird.cn',
 }
-if (typeof process !== 'undefined' && process.env && process.env['NODE_ENV'] === 'development') {
-    _basePath = '';
-    _apiUrl = 'http://localhost:9001/api';
-    _dcConfig = {
-        wssUrl: 'ws://192.168.31.31:9944',
-        backWssUrl: 'ws://192.168.31.31:9944',
-    }
-}
+// if (typeof process !== 'undefined' && process.env && process.env.NODE_ENV === 'development') {
+//     _basePath = '';
+//     _apiUrl = 'http://localhost:9001/api';
+//     _dcConfig = {
+//         wssUrl: 'ws://192.168.31.31:9944',
+//         backWssUrl: 'ws://192.168.31.31:9944',
+//     }
+// }
 export const basePath = _basePath;
 export const apiUrl = _apiUrl;
 export const dcConfig = _dcConfig;

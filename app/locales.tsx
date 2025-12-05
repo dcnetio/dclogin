dbb2f3a74da9519646f0f09df1220245f939d355
wangqin <752935464@qq.com>
2025-12-05 14:35:24 +0800

    严格模式支持

--- app/locales.tsx
+++ app/locales.tsx
@@ -1,9 +1,6 @@
 "use client";
-import { useEffect } from "react";
-import i18next from "i18next";
-import { I18nextProvider } from 'react-i18next';
+import { I18nextProvider } from "react-i18next";
 import i18n from "@/locales/i18n";
-import { I18N_LANGUAGES } from '@/config/constant';
 
 export default function Locales({
   children,
